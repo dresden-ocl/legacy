af398531d41619b1db8655dd08ba7d92df2f0e1f
dblcdblu <dblcdblu@17acace0-ed7c-4735-91e3-bdb5566e35f9>
2008-12-10 07:48:49 +0000

    Tagged Release Dresden OCL2 for Eclipse 1.1.

---
+++ tags/dresden_ocl2_for_eclipse_1.1/eclipse/tudresden.ocl20.pivot.modelbus/src/tudresden/ocl20/pivot/modelbus/internal/ModelRegistry.java
@@ -0,0 +1,249 @@
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (C) 2007 Matthias Braeuer (braeuer.matthias@web.de).            *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This work was done as a project at the Chair for Software Technology,     *
+ * Dresden University Of Technology, Germany (http://st.inf.tu-dresden.de).  *
+ * It is understood that any modification not identified as such is not      *
+ * covered by the preceding statement.                                       *
+ *                                                                           *
+ * This work is free software; you can redistribute it and/or modify it      *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation; either version 2 of the License, or      *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This work is distributed in the hope that it will be useful, but WITHOUT  *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     *
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public     *
+ * License for more details.                                                 *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public License *
+ * along with this library; if not, you can view it online at                *
+ * http://www.fsf.org/licensing/licenses/gpl.html.                           *
+ *                                                                           *
+ * To submit a bug report, send a comment, or get the latest news on this    *
+ * project, please visit the website: http://dresden-ocl.sourceforge.net.    *
+ * For more information on OCL and related projects visit the OCL Portal:    *
+ * http://st.inf.tu-dresden.de/ocl                                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * $Id$
+ */
+package tudresden.ocl20.pivot.modelbus.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.ListenerList;
+import org.eclipse.osgi.util.NLS;
+
+import tudresden.ocl20.pivot.modelbus.IModel;
+import tudresden.ocl20.pivot.modelbus.IModelRegistry;
+import tudresden.ocl20.pivot.modelbus.ModelBusPlugin;
+import tudresden.ocl20.pivot.modelbus.event.IModelRegistryListener;
+import tudresden.ocl20.pivot.modelbus.event.ModelRegistryEvent;
+
+/**
+ * Standard implementation of the {@link IModelRegistry} interface that uses a simple list for
+ * storing references to {@link IModel models}.
+ * 
+ * @author Matthias Braeuer
+ * @version 1.0 11.04.2007
+ */
+public class ModelRegistry implements IModelRegistry {
+
+  // Logger for this class
+  private static final Logger logger = ModelBusPlugin.getLogger(ModelRegistry.class);
+
+  // the list of registered models
+  private List<IModel> models;
+
+  // keeps track of the active model
+  private IModel activeModel;
+
+  // a list of listeners
+  private ListenerList listeners;
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see tudresden.ocl20.pivot.modelbus.IModelRegistry#addMetamodel(tudresden.ocl20.pivot.modelbus.IModel)
+   */
+  public void addModel(IModel model) {
+    if (logger.isDebugEnabled()) {
+      logger.debug("addModel(model=" + model + ") - enter"); //$NON-NLS-1$ //$NON-NLS-2$
+    }
+
+    if (model == null) {
+      throw new IllegalArgumentException("The parameter 'model' must not be null."); //$NON-NLS-1$
+    }
+
+    // lazily create the list of models
+    if (models == null) {
+      models = new ArrayList<IModel>();
+    }
+
+    // check if model is already contained in the registry; this is meant to be captured and dealt
+    // with on the UI, e.g., by showing an error message; this is better than silently do nothing
+    if (models.contains(model)) {
+      throw new IllegalStateException("Model '" + model.getDisplayName() + "' is already loaded."); //$NON-NLS-1$//$NON-NLS-2$
+    }
+
+    // add the model
+    models.add(model);
+
+    // inform listeners
+    fireModelAdded(model);
+
+    // if no model has been active, make this the active one
+    if (activeModel == null) {
+      setActiveModel(model);
+    }
+
+    if (logger.isDebugEnabled()) {
+      logger.debug("addModel() - exit"); //$NON-NLS-1$
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see tudresden.ocl20.pivot.modelbus.IModelRegistry#getActiveModel()
+   */
+  public IModel getActiveModel() {
+    return activeModel;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see tudresden.ocl20.pivot.modelbus.IModelRegistry#setActiveModel(tudresden.ocl20.pivot.modelbus.IModel)
+   */
+  public void setActiveModel(IModel model) {
+    if (logger.isDebugEnabled()) {
+      logger.debug("setActiveModel(model=" + model + ") - enter"); //$NON-NLS-1$ //$NON-NLS-2$
+    }
+
+    if (model == null) {
+      throw new IllegalArgumentException("The active model must not be null."); //$NON-NLS-1$
+    }
+
+    // check that the new active model is managed by this registry
+    if (!models.contains(model)) {
+      throw new IllegalArgumentException("The model '" + model.getDisplayName() //$NON-NLS-1$
+          + "' must be added to the model registry first before it can be set active."); //$NON-NLS-1$
+    }
+
+    // only update if the active model has changed
+    if (activeModel == null || !activeModel.equals(model)) {
+      if (logger.isInfoEnabled()) {
+        logger.info(NLS.bind(ModelBusMessages.ModelRegistry_SettingActiveModel,model
+            .getDisplayName()));
+      }
+
+      activeModel = model;
+      fireActiveModelChanged(model);
+    }
+
+    if (logger.isDebugEnabled()) {
+      logger.debug("setActiveModel() - exit"); //$NON-NLS-1$
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see tudresden.ocl20.pivot.modelbus.IModelRegistry#dispose()
+   */
+  public void dispose() {
+    if (models != null) {
+      models.clear();
+      models = null;
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see tudresden.ocl20.pivot.modelbus.IModelRegistry#getModels()
+   */
+  public IModel[] getModels() {
+    return models == null ? new IModel[] {} : models.toArray(new IModel[models.size()]);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see tudresden.ocl20.pivot.modelbus.IModelRegistry#addModelBusListener(tudresden.ocl20.pivot.modelbus.event.IModelRegistryListener)
+   */
+  public void addModelRegistryListener(IModelRegistryListener listener) {
+    getListeners().add(listener);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see tudresden.ocl20.pivot.modelbus.IModelRegistry#removeModelBusListener(tudresden.ocl20.pivot.modelbus.event.IModelRegistryListener)
+   */
+  public void removeModelRegistryListener(IModelRegistryListener listener) {
+    if (listeners != null) {
+      listeners.remove(listener);
+    }
+  }
+
+  /**
+   * Helper method that informs all listeners about an added model.
+   */
+  protected void fireModelAdded(IModel model) {
+    ModelRegistryEvent event = null;
+
+    if (listeners != null) {
+      Object[] listeners = this.listeners.getListeners();
+
+      for (int i = 0; i < listeners.length; i++) {
+
+        // lazily create the event
+        if (event == null) {
+          event = new ModelRegistryEvent(this,model);
+        }
+
+        ((IModelRegistryListener) listeners[i]).modelAdded(event);
+      }
+    }
+  }
+
+  /**
+   * Helper method that informs all listeners about an added model.
+   */
+  protected void fireActiveModelChanged(IModel model) {
+    ModelRegistryEvent event = null;
+
+    if (listeners != null) {
+      Object[] listeners = this.listeners.getListeners();
+
+      for (int i = 0; i < listeners.length; i++) {
+
+        // lazily create the event
+        if (event == null) {
+          event = new ModelRegistryEvent(this,model);
+        }
+
+        ((IModelRegistryListener) listeners[i]).activeModelChanged(event);
+      }
+    }
+  }
+
+  /**
+   * Helper method that lazily creates the list of listeners.
+   */
+  protected ListenerList getListeners() {
+
+    if (listeners == null) {
+      listeners = new ListenerList(ListenerList.IDENTITY);
+    }
+
+    return listeners;
+  }
+}
---
+++ tags/dresden_ocl2_for_eclipse_1.1/eclipse/tudresden.ocl20.pivot.ocl2parser.test/src/tudresden/ocl20/pivot/ocl2parser/test/parsertests/OclFilesTest.java
@@ -0,0 +1,232 @@
+/*
+    Copyright (C) 2007  Nils (s0006383@inf.tu-dresden.de)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package tudresden.ocl20.pivot.ocl2parser.test.parsertests;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+import tudresden.ocl20.pivot.modelbus.IMetamodel;
+import tudresden.ocl20.pivot.modelbus.IModel;
+import tudresden.ocl20.pivot.modelbus.IModelProvider;
+import tudresden.ocl20.pivot.modelbus.ModelAccessException;
+import tudresden.ocl20.pivot.modelbus.ModelBusPlugin;
+import tudresden.ocl20.pivot.ocl2parser.parser.OCL2Parser;
+import tudresden.ocl20.pivot.pivotmodel.Namespace;
+import tudresden.ocl20.pivot.pivotmodel.Operation;
+import tudresden.ocl20.pivot.pivotmodel.Type;
+
+/**
+ * This testcase tests the parser by letting the parser parse all test data files
+ * that are in the directory <i>testdata</i>. For each file one test method exists,
+ * so if an error occurs we can located the file in which the error occurred.
+ * All messages are written to the console.
+ * @author Nils
+ *
+ */
+public class OclFilesTest extends TestCase {
+	public void testOclFile1() {
+		String fileName = "oclTestFiles/oclFile01.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.setUMLModel("LoyalRoyalOCL2Parser_4.xmi");
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	public void testOclFile2() {
+		String fileName = "oclTestFiles/oclFile02.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	public void testOclFile3() {
+		String fileName = "oclTestFiles/oclFile03.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	public void testOclFile4() {
+		String fileName = "oclTestFiles/oclFile04.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	public void testOclFile5() {
+		String fileName = "oclTestFiles/oclFile05.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	/*public void testOclFile6() {
+		String fileName = "oclTestFiles/oclFile06.ocl";
+		try {
+			OclFileTest test = OclFileTest.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}*/
+	
+	public void testOclFile7() {
+		String fileName = "oclTestFiles/oclFile07.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	public void testOclFile8() {
+		String fileName = "oclTestFiles/oclFile08.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	public void testOclFile9() {
+		String fileName = "oclTestFiles/oclFile09.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	public void testOclFile10() {
+		String fileName = "oclTestFiles/oclFile10.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}
+	
+	public void testOclFile11() {
+		String fileName = "oclTestFiles/oclFile11.ocl";
+		try {
+			TestPerformer test = TestPerformer.getDefault();
+			test.parseFile(fileName);
+		} catch(Throwable ex) {
+			String message = " This error occured for file " + fileName + ".";
+			System.err.println(message);
+			ex.printStackTrace();
+			
+			fail();
+			return;
+		}
+		
+		assertTrue(true);
+	}	
+}
