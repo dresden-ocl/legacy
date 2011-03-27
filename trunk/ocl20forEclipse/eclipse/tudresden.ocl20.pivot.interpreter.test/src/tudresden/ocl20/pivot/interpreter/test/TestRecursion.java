f3dd247d5ce9153ee27f9cc5cd6019211f886d8e
cwilke <cwilke@17acace0-ed7c-4735-91e3-bdb5566e35f9>
2011-03-27 13:08:17 +0000

    Added interpreter tests to headless test suite.

--- trunk/ocl20forEclipse/eclipse/tudresden.ocl20.pivot.interpreter.test/src/tudresden/ocl20/pivot/interpreter/test/TestRecursion.java
+++ trunk/ocl20forEclipse/eclipse/tudresden.ocl20.pivot.interpreter.test/src/tudresden/ocl20/pivot/interpreter/test/TestRecursion.java
@@ -26,8 +26,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import tudresden.ocl20.pivot.essentialocl.standardlibrary.OclInteger;
@@ -48,36 +46,6 @@ public class TestRecursion extends AbstractInterpreterTest {
 
 	/** The name of the constraint directory for this test suite. */
 	private static final String CONSTRAINT_DIRECTORY = "recursion";
-
-	/**
-	 * <p>
-	 * Initializes the test cases.
-	 * </p>
-	 * 
-	 * @throws ModelAccessException
-	 * @throws IllegalArgumentException
-	 */
-	@BeforeClass
-	public static void setUp() throws IllegalArgumentException,
-			ModelAccessException {
-
-		AbstractInterpreterTest.setUp();
-	}
-
-	/**
-	 * <p>
-	 * Tears down the test cases.
-	 * </p>
-	 * 
-	 * @throws ModelAccessException
-	 * @throws IllegalArgumentException
-	 */
-	@AfterClass
-	public static void tearDown() throws IllegalArgumentException,
-			ModelAccessException {
-
-		AbstractInterpreterTest.tearDown();
-	}
 
 	/**
 	 * <p>
